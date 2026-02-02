07cf801c7a5679a36f2d84fdb024920166a40021
Haragos99 <geri.haragos@gmail.com>
2026-02-02 21:49:01 +0100

    add coment

--- run.py
+++ run.py
@@ -2,6 +2,11 @@ import sys
 import importlib
 
 DEV_SRC = r"PATH_TO_YOUR_DEV_SRC_DIRECTORY"
+""" 
+This is run only in development environment Open in Maya's script editor and run this file to reload the src files
+"""
+
+# Adjust the path to your local src folder
 if DEV_SRC not in sys.path:
     sys.path.append(DEV_SRC)
 
